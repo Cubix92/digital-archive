428305ddd9c4b00127c3bc5d466b3c1f0c87e08f
jakub.ksiazek <jakub.ksiazek@mediaflex.pl>
2017-12-05 12:33:19 +0100

    translate/msg fix

--- module/Application/src/Controller/TagController.php
+++ module/Application/src/Controller/TagController.php
@@ -37,7 +37,7 @@ class TagController extends AbstractActionController
         try {
             $tag = $this->tagRepository->findById($id);
         } catch(\UnexpectedValueException $e) {
-            $this->flashMessenger()->addErrorMessage(sprintf('Tag with identifier not found');
+            $this->flashMessenger()->addErrorMessage('Tag with identifier not found');
             return $this->redirect()->toRoute('tag', ['action' => 'index']);
         }
 
